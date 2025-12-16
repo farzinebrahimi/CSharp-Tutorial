d2fc7dc0c15bad86fb848bb99c45b1805b1ff7e1
Farzin Ebrahimi <Farzinebrahimi.info@gmail.com>
2025-12-16 19:53:43 +0330

    feat:
    -Create a new project.
    -Create a app for show person info.

---
+++ Vira-Tutorial/Vira-Tutorial/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Please enter your name:");
+string name = Console.ReadLine();
+
+Console.WriteLine("Please enter your last name:");
+string lastName = Console.ReadLine();
+
+Console.WriteLine("Please enter your age:");
+int age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Your name is {name} {lastName} and your age is {age}");
+Console.WriteLine("Your name is" + name + " " + lastName +  " " + "and your age is" + " " + age);
+
+
+
+
+
